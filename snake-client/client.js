f7fa577397e75da310d31b5aa31df4acf1a6c24b
amoham1 <amoham456@gmail.com>
2021-11-03 18:29:28 -0400

    add the event input setup and user handle input

---
+++ snake-client/client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243', // IP address here,
+    port: '50541' // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", (data) => {
+    // code that does something when the connection is first established
+    console.log("Successfully connected to game server");
+    conn.write('Name: SIG');
+  });
+  conn.on("connect", (data) => {
+    //conn.write("Move: up");
+  });
+
+  return conn;
+};
+
+module.exports = connect;
