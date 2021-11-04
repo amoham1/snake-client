cdd7a0e90f25dbdbc269987838b4005e5485e993
amoham1 <amoham456@gmail.com>
2021-11-03 20:14:17 -0400

    made my snake move

--- snake-client/play.js
+++ snake-client/play.js
@@ -1,16 +1,6 @@
-const net = require("net");
-
 const connect = require("./client");
 const setupInput = require("./input");
 
 
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
 console.log("Connecting ...");
-connect();
-setupInput(handleUserInput);
+setupInput(connect());
